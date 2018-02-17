8e86c59c8514f7adcf0c737d3255e1f7c3a83b82
Pedro Rabinovitch <pedromr@gmail.com>
2018-02-16 22:49:21 -0500

    Moving spawn center file

---
+++ Shmup/Assets/Shmup/Scripts/SpawnCenter.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace com.pedromr.games.shmup
+{
+	public class SpawnCenter : MonoBehaviour
+	{
+		private bool spawned = false;
+
+		private EnemySpawn[] spawnChildren;
+
+
+		public void Start() {
+			spawnChildren = GetComponentsInChildren<EnemySpawn>();
+			Debug.Log("Squad online, children: "+spawnChildren.Length);
+		}
+
+		public void OnDrawGizmos()
+		{
+			Gizmos.color = Color.yellow;
+			//if (!DrawEditorMeshForObject()) 
+			{
+				Gizmos.DrawWireSphere(transform.position, 1);
+			}
+			//DrawEnemyParams(enemyParams);
+		}
+
+
+		private void OnTriggerEnter(Collider other)
+		{
+			spawnChildren = GetComponentsInChildren<EnemySpawn>();
+			if (!spawned && other.gameObject.CompareTag("GameController")) {
+				Debug.Log("Spawning squadron, children: "+spawnChildren.Length);
+				foreach (var spawnee in spawnChildren) {
+					spawnee.Spawn();
+				}
+				Debug.Log("Spawning squadron: Done");
+			}
+		}
+
+
+	}
+}
